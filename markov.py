79fe8ede6ef1db95d4fbb226d8c512fc7674a8fc
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-31 20:35:44 -0800

    can now enter filename as argument

--- markov.py
+++ markov.py
@@ -1,6 +1,8 @@
 """Generate Markov text from text files."""
 
 from random import choice
+import sys
+
 
 
 def open_and_read_file(file_path):
@@ -79,7 +81,7 @@ def make_text(chains):
     return " ".join(words)
 
 
-input_path = "gettysburg.txt"
+input_path = sys.argv[1]
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
